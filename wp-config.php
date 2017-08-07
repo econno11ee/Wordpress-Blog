d747fe564584d5e22a8b26c6c1dc1fdad43e5d1f
StarTrek Seeber <nick@techtonicgroup.com>
2017-08-07 15:01:56 -0600

    Angular added to homepage template file

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp_mhi');
+define('DB_NAME', 'wordpresstechtonic');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'techtonic');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 't3cht0n!c');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'vpn.techtonicgroup.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
--- wp-content/themes/twentyseventeen/HomePage.php
+++ wp-content/themes/twentyseventeen/HomePage.php
@@ -5,26 +5,46 @@ include_once( ABSPATH . '/wp-content/plugins/Techtonic-WPPlugin/utils/DataAccess
 
 global $wpdb;
 $user = wp_get_current_user();
-$Data = new DataAccess();
-$testData = $Data::GetHomepage();
+
 ?>
 
 <?php get_header();?>
-<div class="col-md-12">
-    <div class="row">
+<div class="col-md-12" ng-app="myShoppingList" ng-controller="myCtrl">
+    <div class="row" ng-repeat="item in products">
         <?php if ( have_posts() ) while ( have_posts() ) :
                     the_post(); ?>            
         <?php the_content(); ?>   
         <?php endwhile; ?>	
 
-        Test
+        <?php echo($testData) ?>
+
+        <span>{{item}}</span>
     </div>
 </div>
 
 <?php get_footer();?>
 
-
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
 <script type="text/javascript">
+var app = angular.module("myShoppingList", []); 
 
+
+app.controller("myCtrl", function($scope, $http) {
+    $scope.products = ["Milk", "Bread", "Cheese"];
+
+
+    $scope.getData = function(callback){
+        $http({ method:'get', url:"http://TGWP.com/homecontroller.php?method=GetHomePage"})
+        .success(function(response){
+            callback(response);
+        })
+    }
+
+    $scope.GetDataCallback = function(data){
+        $scope.products = data;
+    }
+
+    $scope.getData($scope.GetDataCallback);
+});
 
 </script>
